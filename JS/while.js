f4e8d71563d5023f3c84bc244744c782b8945309
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-04-19 18:32:49 -0500

    corrected logging error on final function of while.js

--- JS/while.js
+++ JS/while.js
@@ -1,29 +1,25 @@
-
-
-
 console.log("-------------------------------------------------------");
 
 let i = 2;
-let n = 65536
+let n = 65536;
 
 while (i <= n) {
-    console.log(i);
-    i *= 2;
+  console.log(i);
+  i *= 2;
 }
-
 
 console.log("-------------------------------------------------------");
 // This is how you get a random number between 50 and 100
 let allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-console.log(allCones);
-
+console.log(`Come get your cones we only have ${allCones} left!`);
 
 do {
-    let bought = Math.floor(Math.random() * 5) + 1
-    console.log(`Customer purchasing ${bought} cones`)
-    console.log(`We only have ${allCones} cones left!`)
-    allCones -= bought
-}while (allCones > Math.floor(Math.random() * 5) + 1){
-    console.log(`I'm sorry we are out off cones`)
+  let bought = Math.floor(Math.random() * 5) + 1;
+  console.log(`Customer purchasing ${bought} cones`);
+  allCones -= bought;
+  console.log(`We only have ${allCones} cones left!`);
+} while (allCones > Math.floor(Math.random() * 5) + 1);
+{
+  console.log(`I'm sorry we are out off cones`);
 }
