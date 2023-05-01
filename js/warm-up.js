f025617c03849006e29c3f8ab537c832c44e04b6
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-05-01 09:42:18 -0500

    JS Warmup May - 1st

--- js/warm-up.js
+++ js/warm-up.js
@@ -1,5 +1,32 @@
-function seven() {
-    return 7
+// function seven() {
+//     return 7
+// }
+//
+// console.log(seven());
+// -----------------------------------------------
+
+
+// Write a function that takes in a number and returns an array of numbers that are divisible within the argument. If the value provided is not a number, the function should return false.
+
+function findFactors(x) {
+    let factors = []
+    if (typeof x === "number") {
+        for (let i = 1; i <= x; i++){
+            if (x % i == 0) {
+                factors.push(i)
+            }
+        }
+    } else return console.log(false)
+
+
+    return console.log(factors)
 }
 
-console.log(seven());
+findFactors(6); // returns [1,2,3,6];
+findFactors(16); // returns [1,2,4,8,16];
+findFactors(0); // returns [];
+findFactors(true); // returns false;
+findFactors("13"); // returns false;
+findFactors([54,72,144]); // returns false;
+findFactors({value: 64}); // returns false;
+findFactors(); // returns false;
