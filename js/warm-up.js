29dfa37576b118c2a969ef414d040264cf824585
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-05-10 10:58:04 -0500

    finalized 0rder-pizza.html

--- js/warm-up.js
+++ js/warm-up.js
@@ -69,9 +69,26 @@
 //
 // fizzBuzz()
 
-let number = 2;
-do {
-  console.log(number);
+// let number = 2;
+// do {
+//   console.log(number);
+//
+//   number *= number;
+// } while (number <= 1000000);
 
-  number *= number;
-} while (number <= 1000000);
+// Write a function that takes in an array of numbers and returns all of the numbers added together (We'll be assuming that only arrays are going to be used with this function).
+
+function addEmUp(x) {
+  let total = 0;
+  for (let i = 0; i < x.length; i++) {
+    total += x[i];
+  }
+  return total;
+}
+
+console.log(addEmUp([2, 6, 19])); // returns 27
+console.log(addEmUp([-99, 180, -5])); // returns 76
+console.log(addEmUp([44, 10, 7])); // returns 61
+console.log(addEmUp([-100])); // returns -100
+console.log(addEmUp([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // returns 55
+console.log(addEmUp([-13, -92, -3500])); // returns -3605
