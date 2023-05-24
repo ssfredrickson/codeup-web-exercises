43af16860c496518da9fc5a12414dab35128a92c
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-05-23 22:43:55 -0500

    finalized JQuery exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,37 +1,40 @@
 "use strict";
 
-
-
 let konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', "Enter"]
-
 let current = 0;
 
+// Handler function
 function cheatCode(event) {
     console.log(event.key);
-    // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+
+    // If event does not match value of index OR position within the array doesn't match the current value of the "current" variable
+
     if (konamiCode.indexOf(event.key) < 0 || event.key !== konamiCode[current]) {
         current = 0;
         return;
     }
 
-    // Update how much of the pattern is complete
+    // Update current variable if event matches indexOf array
+
     current++;
 
-    // If complete, alert and reset
+    // If length of array matches value of "current" variable reset "current" value and manipulate the HTML w/ JQuery
+
     if (konamiCode.length === current) {
         current = 0;
         $('#main-title').html("WELCOME BACK")
         $("#lives").html("30")
         $('#lives').toggleClass('zero-lives lives')
         $('#game-over').css("display", "none")
-        $('#combatText').html("&#9798 SOMEONE HAS BEEN LOOKING OUT FOR YOU &#9798")
+        $('#combatText').html("&#9798 LOOKS LIKE SOMEONE IS WATCHING OUT FOR YOU &#9798")
         $('#continue').html("PRESS <span class=\"material-symbols-outlined\">\n" +
             "space_bar\n" +
             "</span> TO CONTINUE")
+        $('#continue').css("margin-top", "20px")
 
     }
 
 }
 
-// Listen for keydown events
+// ------------- JQuery Event Listener on "keyup" -----------------
 $(document).on('keyup', cheatCode);
