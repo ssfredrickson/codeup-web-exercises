d7a09fe66ca6b79c170c49205abe50c4e9af22f3
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-05-30 18:42:13 -0500

    Final Draft Mapbox Exercise

--- js/mapbox.js
+++ js/mapbox.js
@@ -4,11 +4,12 @@ var map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v11", // style URL
   zoom: 10, // starting zoom
   center: [-98.1261, 29.700345], // [lng, lat]
+
 });
 
-let restaurants = [
+let places = [
   {
-    name: "Huisache",
+    name: "Huisache Grill",
     location: [-98.1261, 29.700345],
     description:
       "An intimate and casual restaurant serving\n" +
@@ -31,16 +32,57 @@ let restaurants = [
   },
 ];
 
+
+let currentMarkers = []
+
 function makeMarkers() {
-  restaurants.forEach(function (items) {
+  places.forEach(function (items) {
     let popup = new mapboxgl.Popup().setHTML(
       `<h3>${items.name}</h3><p>${items.description}</p><a href=${items.menuLink} target='_blank'>Menu</a>`
     );
-    let marker = new mapboxgl.Marker()
-      .setLngLat(items.location)
-      .addTo(map)
-      .setPopup(popup);
+      var marker = new mapboxgl.Marker()
+          .setLngLat(items.location)
+          .addTo(map)
+          .setPopup(popup);
+
+      currentMarkers.push(marker)
   });
+
 }
 
+function removeMarkers() {
+   if (currentMarkers !== null) {
+       for (let i = currentMarkers.length - 1; i >= 0; i--)
+           currentMarkers[i].remove()
+   }
+}
+
+let userName = document.querySelector("#name");
+let zip = document.querySelector('#zip')
+
+function addUserMarker() {
+    let popup = new mapboxgl.Popup().setHTML(
+        `<h3>${userName.value}'s Location</h3>`
+    );
+    let userLocation = geocode(`${zip.value}`, myMapboxKey)
+    userLocation.then(function (data) {
+        var marker = new mapboxgl.Marker()
+            .setLngLat(data)
+            .addTo(map)
+            .setPopup(popup);
+        map.flyTo({
+            center: data,
+            essential:true,
+        })
+
+        currentMarkers.push(marker)
+    })
+}
+
+
+document.querySelector('#zip-button').addEventListener('click', addUserMarker)
+
 document.querySelector("#add-markers").addEventListener("click", makeMarkers);
+
+document.querySelector("#remove-markers").addEventListener("click", removeMarkers)
+
