f5caa5ece18166aa9a817b76eb019de0d90958b7
Stephen Fredrickson <stephen.fredrickson@gmail.com>
2023-06-01 17:22:30 -0500

    final Weather Map Commit

--- js/keys.js
+++ js/keys.js
@@ -1,5 +1,3 @@
 let MY_MAPBOX_KEY = 'pk.eyJ1Ijoic3NmcmVkcmlja3NvbiIsImEiOiJjbGlhZTI2dXQwMmF5M29xdTcyaDVjbXJ5In0.5iKRRxBlruDDGdS1ftUwDQ'
 
 let MY_OWM_KEY = "ae36d1b5c6b7963f8bf1c473aba46862";
-
-let FIREBASE_KEY = "AIzaSyAcEXBJSzaIOhWObkVFnbcJeVuf-N7eV08"
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,9 +1,8 @@
 (function () {
   "use strict";
-// ------------------Map Starting Coordinates --------------------
+  // ------------------Map Starting Coordinates --------------------
 
-
-  let myLatLong = [-97.371118, 30.349508];
+  let myLatLong = [-97.371118, 30.349508]; //Elgin,TX
 
   mapboxgl.accessToken = MY_MAPBOX_KEY;
   const map = new mapboxgl.Map({
@@ -22,135 +21,99 @@
     .setLngLat(myLatLong)
     .addTo(map);
 
-  // ------------------------- Function #1 Get Weather Data on Load ----------------
 
-  $(window).on("load", function () {
-    console.log("Function #1");
-    $.get(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${myLatLong[1]}&lon=${myLatLong[0]}&units=imperial&appid=${MY_OWM_KEY}`
-    ).done(function (data) {
-      console.log(data);
+  // --------------------function to convert degrees to Cardinal Directions ----------
 
-      $("#card-container").empty();
-      $("#header")
-        .empty()
-        .append(`<h1 class="text-info">${data.city.name}</h1>`);
-      data.list.forEach(function (item, index) {
-        let milliseconds = (item.dt * 1000)
-        if (index % 8 === 0) {
-          $("#card-container").append(`<div class="card col">
+  function degToCompass(num) {
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
+  }
+
+
+  // ------------------------- Create 5 Weather Cards Function ----------------
+
+  const cardHTML = (data) => {
+  marker.setPopup(new mapboxgl.Popup({closeButton: false}).setHTML(`<h6>Wind: ${degToCompass(data.list[0].wind.deg)}</h6><h6>${(data.list[0].wind.speed).toFixed(0)} MPH</h6>`))
+    data.list.forEach(function (item, index) {
+      let milliseconds = item.dt * 1000;
+      if (index % 8 === 0) {
+        let weatherIconCode = item.weather[0].icon
+        $("#card-container").append(`<div class="card col bg-dark" id="weather-cards">
   <div class="card-header">
     <h6>${new Date(milliseconds).toDateString()}</h6>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">Temp: ${item.main.temp.toFixed(0)}&#8457</li>
+   <li class="list-group-item"><img id="weather-icon" src="http://openweathermap.org/img/w/${weatherIconCode}.png"> ${item.main.temp.toFixed(0)}&#8457</li>
     <li class="list-group-item">Feels Like: ${item.main.feels_like.toFixed(
       0
     )}&#8457</li>
-    <li class="list-group-item">Chance of Rain: ${(item.pop * 100).toFixed(
+    <li class="list-group-item">Rain Chance: ${(item.pop * 100).toFixed(
       0
     )} % </li>
   </ul>
 </div>`);
-        } else {
-        }
-      });
+      }
+    });
+
+  };
+
+  // --------------------------Get Weather data and create cards on load--------------------
+
+  $(window).on("load", function () {
+    $.get(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${myLatLong[1]}&lon=${myLatLong[0]}&units=imperial&appid=${MY_OWM_KEY}`
+    ).done(function (data) {
+      $("#card-container").empty();
+      $("#header")
+        .empty()
+        .html(`<h1 class="text-dark">${data.city.name}</h1>`);
+      cardHTML(data);
     });
   });
 
-  // ------------------------Function #2 Get Weather Data when marker stops dragging -----------
+  // ------------------------Get Weather Data when marker stops dragging update cards -----------
 
   function onDragEnd() {
     updatedCoordinates = [];
     const lngLat = marker.getLngLat();
     updatedCoordinates.push(lngLat.lng);
     updatedCoordinates.push(lngLat.lat);
-    console.log(updatedCoordinates);
-
     $.get(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${updatedCoordinates[1]}&lon=${updatedCoordinates[0]}&units=imperial&appid=${MY_OWM_KEY}`
     ).done(function (data) {
-      console.log('Function #2');
-
       $("#card-container").empty();
-      $("#header").empty().append(`<h1 class="text-info">${data.city.name}</h1>`);
-      data.list.forEach(function (item, index) {
-        let milliseconds = (item.dt * 1000)
-        if (index % 8 === 0) {
-          $("#card-container")
-            .append(`<div class="card col">
-  <div class="card-header">
-    <h6>${new Date(milliseconds).toDateString()}</h6>
-  </div>
-  <ul class="list-group list-group-flush">
-   <li class="list-group-item">Temp: ${item.main.temp.toFixed(0)}&#8457</li>
-    <li class="list-group-item">Feels Like: ${item.main.feels_like.toFixed(
-      0
-    )}&#8457</li>
-    <li class="list-group-item">Chance of Rain: ${(item.pop * 100).toFixed(
-      0
-    )} % </li>
-  </ul>
-</div>`);
-        }
-      });
+      $("#header")
+        .empty()
+        .append(`<h1 class="text-dark">${data.city.name}</h1>`);
+      cardHTML(data);
     });
   }
-
   marker.on("dragend", onDragEnd);
 
   let searchLocation = document.querySelector("#location-search");
 
+  // ------------------------- Get weather data from search bar and update cards----------------
+
   $("#location-search-button").on("click", function (event) {
     event.preventDefault();
-    console.log('Function #3');
     marker.remove();
     let userLocation = geocode(`${searchLocation.value}`, MY_MAPBOX_KEY);
-
     userLocation.then(function (data) {
       marker.setLngLat(data).addTo(map);
-      console.log(data)
       map.flyTo({
         center: data,
         essential: true,
       });
-
       $.get(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${data[1]}&lon=${data[0]}&units=imperial&appid=${MY_OWM_KEY}`
       ).done(function (data) {
-        console.log(data);
         $("#card-container").empty();
         $("#header")
           .empty()
-          .append(`<h1 class="text-info">${data.city.name}</h1>`);
-
-        data.list.forEach(function (item, index) {
-          let milliseconds = (item.dt * 1000)
-          if (index % 8 === 0) {
-            $("#card-container").append(`<div class="card col">
-  <div class="card-header">
-    <h6>${new Date(milliseconds).toDateString()}</h6>
-  </div>
-  <ul class="list-group list-group-flush">
-   <li class="list-group-item">Temp: ${item.main.temp.toFixed(
-     0
-   )}&#8457</li>
-    <li class="list-group-item">Feels Like: ${item.main.feels_like.toFixed(
-      0
-    )}&#8457</li>
-    <li class="list-group-item">Chance of Rain: ${(
-      item.pop * 100
-    ).toFixed(0)} % </li>
-  </ul>
-</div>`);
-          }
-        });
+          .append(`<h1 class="text-dark">${data.city.name}</h1>`);
+        cardHTML(data);
       });
     });
   });
-
-
-
-
-
 })();
